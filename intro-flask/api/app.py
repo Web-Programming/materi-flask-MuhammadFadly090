c8a4bdc7c9d39730a3f4e7daa27c1434ba62bee2
MuhammadFadly090 <161425365+MuhammadFadly090@users.noreply.github.com>
2024-11-21 19:38:44 +0700

    upadate

---
+++ intro-flask/api/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+# Ganti dengan kredensial MySQL Anda
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost/myflask'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from routes import *
+
+if __name__ == "__main__":
+    app.run(debug=True)
